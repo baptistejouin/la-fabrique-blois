302033c098bc853b7a92167d9d5e0e730ad94721
baptistejouin <baptiste.jouin44@gmail.com>
2022-03-17 15:21:11 +0100

    Fix navbar style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,14 +3,12 @@
 		<router-link to="/" class="navbar__logo">
 			<img src="@/assets/img/logos/logo_fabrique.svg" />
 		</router-link>
-		<div
-			class="navbar__full"
-			:class="{ 'navbar__full--mobile-active': mobileExpended }"
-			:tabindex="{ '-1': mobileExpended }"
-			:aria-hidden="!mobileExpended"
-			:aria-expanded="mobileExpended"
-		>
-			<button class="navbar__mobile-only navbar__close" @click="toggleMobileNav()">
+		<div class="navbar__full" :class="{ 'navbar__full--mobile-active': mobileExpended }">
+			<button
+				class="navbar__mobile-only navbar__close"
+				@click="toggleMobileNav()"
+				aria-label="Toggle mobile navbar"
+			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -48,24 +46,10 @@
 		</div>
 		<div class="navbar__mobile-only">
 			<button class="navbar__menu" @click="toggleMobileNav()">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					xmlns:xlink="http://www.w3.org/1999/xlink"
-					aria-hidden="true"
-					role="img"
-					width="32"
-					height="32"
-					preserveAspectRatio="xMidYMid meet"
-					viewBox="0 0 24 24"
-				>
-					<path
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 12h16M4 6h16M4 18h16"
-					/>
+				<svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<line y1="1" x2="25" y2="1" stroke="black" stroke-width="2" />
+					<line y1="11" x2="25" y2="11" stroke="black" stroke-width="2" />
+					<line y1="21" x2="25" y2="21" stroke="black" stroke-width="2" />
 				</svg>
 				Menu
 			</button>
