9bea8150d07665e98ac1222747a59435399820dc
AlexianBoccanfuso <alexianboccanfuso@yahoo.fr>
2022-03-22 12:22:50 +0100

    réglages mise en ligne

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    },
+    publicDir: "./",
+    root: "./",
+    base: "./",
+
 })
